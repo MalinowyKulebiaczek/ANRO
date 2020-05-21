c95b538850e192c19fd68515c2bc30e3b61b147c
MalinowyKulebiaczek <50968224+MalinowyKulebiaczek@users.noreply.github.com>
2020-05-21 12:33:40 +0200

    wersja finalna

--- lab5/scripts/ikin.py
+++ lab5/scripts/ikin.py
@@ -23,6 +23,7 @@ global prev_theta2
 global prev_theta4
 def callback(data):
     error=0
+    warn_string=""
     global prev_theta
     global prev_theta2
     global prev_theta4
@@ -38,6 +39,7 @@ def callback(data):
     
     if (- a1**4 + 2*a1**2*a2**2 + 2*a1**2*alpha**2 + 2*a1**2*r34**2 - a2**4 + 2*a2**2*alpha**2 + 2*a2**2*r34**2 - alpha**4 - 2*alpha**2*r34**2 - r34**4)<0:
         theta2=prev_theta2
+        error=2
     else:
         theta2 = -2*atan((2*a1*r34 + (- a1**4 + 2*a1**2*a2**2 + 2*a1**2*alpha**2 + 2*a1**2*r34**2 - a2**4 + 2*a2**2*alpha**2 + 2*a2**2*r34**2 - alpha**4 - 2*alpha**2*r34**2 - r34**4)**(0.5))/(a1**2 + 2*a1*alpha - a2**2 + alpha**2 + r34**2))
         prev_theta2=theta2+pi/2
@@ -45,10 +47,13 @@ def callback(data):
     
     if (- a1**2 + 2*a1*a2 - a2**2 + alpha**2 + r34**2)*(a1**2 + 2*a1*a2 + a2**2 - alpha**2 - r34**2)<0:
         theta4=prev_theta4
+        error=2
     else:  
         theta4 = -2*atan((2*a2*r34 - ((- a1**2 + 2*a1*a2 - a2**2 + alpha**2 + r34**2)*(a1**2 + 2*a1*a2 + a2**2 - alpha**2 - r34**2))**(0.5))/(- a1**2 + a2**2 + 2*a2*alpha + alpha**2 + r34**2))
         prev_theta4=theta4
  
+    if error==2:
+        warn_string="Pozycja koncowki niemozliwa do osiagniecia \n"
     theta3 = theta4 - theta2
     
     theta_vec=[theta1, theta2, theta3]
@@ -58,18 +63,21 @@ def callback(data):
         
         if restrictions[i]['backward']>theta_vec[i]:
             theta_vec[i]=restrictions[i]['backward']
-            warn_string="Przekroczono ograniczenie dolne stawu o numerze: "+str(i+1)
+            warn_string=warn_string+"Przekroczono ograniczenie dolne stawu o numerze: "+str(i+1) +"\n"
             error=1
-            rospy.logerr(warn_string)
+            #rospy.logerr(warn_string)
         elif restrictions[i]['forward']<theta_vec[i]:
             theta_vec[i]=restrictions[i]['forward']
-            warn_string="Przekroczono ograniczenie gorne stawu o numerze: "+str(i+1)
-            rospy.logerr(warn_string)
+            warn_string=warn_string+"Przekroczono ograniczenie gorne stawu o numerze: "+str(i+1)+"\n"
+            #rospy.logerr(warn_string)
             error=1
         i=i+1
     
     if error==1:
         theta_vec=prev_theta
+        rospy.logerr(warn_string)
+    elif error==2:
+        rospy.logerr(warn_string)
     else:
         prev_theta=theta_vec
         
